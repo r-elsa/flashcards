8289f057bc810a4a2cc51553213a61b73d5065b6
Elsa <elsaryynanen@outlook.com>
2023-03-29 15:07:37 +0300

    entities added

---
+++ src/entities/user.py
@@ -0,0 +1,9 @@
+
+class User:
+    def __init__(self, user, username, password,profile_picture=None):
+        self.user = user
+        self.username = username
+        self.password = password
+        self.profile_picture = profile_picture
+
+
--- tasks.py
+++ tasks.py
@@ -7,3 +7,12 @@ def foo(ctx):
 @task
 def start(ctx):
     ctx.run("python3 src/index.py", pty=True)
+
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest", pty=True)
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html", pty=True)
