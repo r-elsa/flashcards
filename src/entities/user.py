445391a3304764385b04a001929a2651ddb66555
Elsa <elsaryynanen@outlook.com>
2023-03-29 15:28:47 +0300

    repositories added

--- src/entities/user.py
+++ src/entities/user.py
@@ -1,9 +1,10 @@
 
 class User:
-    def __init__(self, user, username, password,profile_picture=None):
+    def __init__(self, user, username, password,profile_picture=None, admin=None):
         self.user = user
         self.username = username
         self.password = password
         self.profile_picture = profile_picture
+        self.admin = admin
 
 
