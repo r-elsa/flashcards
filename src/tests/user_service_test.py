a33081761d152c4717b5f813870205d53682ded3
r-elsa <41284745+r-elsa@users.noreply.github.com>
2023-04-04 22:56:06 +0300

    Create user_service_test.py

---
+++ src/tests/user_service_test.py
@@ -0,0 +1,19 @@
+
+import unittest
+from services.user_service import UserService
+
+
+class TestUserService(unittest.TestCase)
+    def setUp(self):
+        print("Set up goes here")
+
+    def user_registered(self):
+   
+        user = Userservice()
+        user.create_user(self, "testname", "testusername", "testpwd123p2")
+        amount_of_users = amount_of_users()
+        self.assertEqual(amount_of_users, 1)
+      
+
+
+     
